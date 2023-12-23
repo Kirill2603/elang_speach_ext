7033df472aa6b8b8c795e96875eb71d0b5e1fd82
kirill2603 <k.radobolsky@gmail.com>
2023-12-23 11:59:32 +0300

    init

---
+++ dist/assets/js/injected.js
@@ -0,0 +1,7 @@
+import { e as exampleThemeStorage } from "./exampleThemeStorage.js";
+async function toggleTheme() {
+  console.log("initial theme", await exampleThemeStorage.get());
+  exampleThemeStorage.toggle();
+  console.log("toggled theme", await exampleThemeStorage.get());
+}
+void toggleTheme();
